6b34962683c808248689b85bcdb3621ad8bd5a53
lizagabriela80-afk <lizagabriela80@gmail.com>
2026-09-10 01:07:54 +0100

    Fix z-index navbar et hero

--- hotel/hotel/settings.py
+++ hotel/hotel/settings.py
@@ -16,7 +16,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['gabrielaliza.pythonanywhere.com']
+ALLOWED_HOSTS = ['gabrielaliza.pythonanywhere.com', '127.0.0.1', 'localhost']
 
 
 # Application definition
