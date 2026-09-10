ff54f797719ab5256f464f2125d53d9fec32c50c
lizagabriela80-afk <lizagabriela80@gmail.com>
2026-09-10 10:19:25 +0100

    fix liste_chambres

--- hotel/rooms/views.py
+++ hotel/rooms/views.py
@@ -2,8 +2,17 @@ from django.shortcuts import render, get_object_or_404
 from .models import Room
 
 def liste_chambres(request):
-    chambres = Room.objects.filter(disponibility=True)
-    return render(request, 'rooms/liste_chambres.html', {'chambres': chambres})
+    chambres_par_categorie = []
+
+    for code, label in Room.ROOM_TYPES:
+        chambres = Room.objects.filter(disponibility=True, type_chambre=code)
+        if chambres.exists():
+            chambres_par_categorie.append({
+                'label': label,
+                'chambres': chambres,
+            })
+
+    return render(request, 'rooms/liste_chambres.html', {'chambres_par_categorie': chambres_par_categorie})
 
 def detail_chambre(request, chambre_id):
     chambre = get_object_or_404(Room, id=chambre_id)
