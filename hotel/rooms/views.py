28d38ac55fea1376d37a069a12a5aaaaca2c1217
lizagabriela80-afk <lizagabriela80@gmail.com>
2026-09-10 13:46:54 +0100

    fix liste_chambres

--- hotel/rooms/views.py
+++ hotel/rooms/views.py
@@ -14,6 +14,7 @@ def liste_chambres(request):
 
     return render(request, 'rooms/liste_chambres.html', {'chambres': chambres})
 
+
 def detail_chambre(request, chambre_id):
     chambre = get_object_or_404(Room, id=chambre_id)
     return render(request, 'rooms/detail_chambre.html', {'chambre': chambre})
