5ed657d4b660d560400b4872008e2560ba5decff
lizagabriela80-afk <lizagabriela80@gmail.com>
2026-09-10 12:56:20 +0100

    fix search_bar

--- hotel/rooms/views.py
+++ hotel/rooms/views.py
@@ -2,17 +2,17 @@ from django.shortcuts import render, get_object_or_404
 from .models import Room
 
 def liste_chambres(request):
-    chambres_par_categorie = []
+    chambres = Room.objects.filter(disponibility=True)
 
-    for code, label in Room.ROOM_TYPES:
-        chambres = Room.objects.filter(type_chambre=code)
-        if chambres.exists():
-            chambres_par_categorie.append({
-                'label': label,
-                'chambres': chambres,
-            })
+    q = request.GET.get('q')
+    if q:
+        chambres = chambres.filter(nom__icontains=q)
 
-    return render(request, 'rooms/liste_chambres.html', {'chambres_par_categorie': chambres_par_categorie})
+    type_chambre = request.GET.get('type_chambre')
+    if type_chambre:
+        chambres = chambres.filter(type_chambre=type_chambre)
+
+    return render(request, 'rooms/liste_chambres.html', {'chambres': chambres})
 
 def detail_chambre(request, chambre_id):
     chambre = get_object_or_404(Room, id=chambre_id)
